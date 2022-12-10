68da01e03ccea2f91c1659e08e07dbf1f45fcfb6
Gauri Bhand <htcgauribhand@gmail.com>
2022-12-10 18:03:47 +0530

    finalCommit

--- src/components/online/Online.jsx
+++ src/components/online/Online.jsx
@@ -1,14 +1,13 @@
-import "./online.css"
+import "./online.css";
 
-export default function Online(user) {
+export default function Online({user}) {
   return (
     <li className="rightbarFriend">
-          <div className="rightbarProfileImgContainer">
-            <img className="rightbarProfileImg" src={user.profilePicture} alt=""/>
-            <span className="rightbarOnline">      
-            </span>
-          </div>
-          <span className="rightbarUsername">{user.username}</span>
-        </li>
-  )
+      <div className="rightbarProfileImgContainer">
+        <img className="rightbarProfileImg" src={user.profilePicture} alt="" />
+        <span className="rightbarOnline"></span>
+      </div>
+      <span className="rightbarUsername">{user.username}</span>
+    </li>
+  );
 }
